c9b4c06465eec90b40e35050fbc1c18b02ea1549
FYLSunghwan <sunghwan@scatterlab.co.kr>
2022-06-06 23:36:12 +0900

    chore: version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="castanets",
-    version="1.0.0",
+    version="1.0.1",
     description="Tool for Multi-stage Review Process",
     install_requires=[
         "python-dotenv",
